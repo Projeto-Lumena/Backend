620e4457e5c41f694a63b20c531c9ded94d4e224
HeloLonghi <heloisa.longhi.ifc@gmail.com>
2026-09-16 09:06:46 -0300

    FEAT: Visualização dos itens da compra na listagem da compra #32

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,7 +2,7 @@ from .avaliacao import AvaliacaoSerializer
 from .categoria import CategoriaSerializer
 from .endereco import EnderecoSerializer
 from .pagamento import PagamentoSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, ItensCompraSerializer
 from .produto_variacao import ProdutoVariacaoSerializer  
 from .produto import ProdutoListSerializer, ProdutoRetrieveSerializer, ProdutoSerializer
 from .tipo import TipoProdutoSerializer
